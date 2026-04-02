0b75b24aa8cc75e8065b9cab9fc94b29391dda18
蕪山 尚 <kabuyamat@domaingrop.grop.co.jp>
2026-04-02 18:49:35 +0700

    Fix: use var for supabase client to avoid CDN conflict

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 const SUPABASE_URL = 'https://ajmdpkwqyeyzemeoojwd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqbWRwa3dxeWV5emVtZW9vandkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMjIwMzAsImV4cCI6MjA5MDY5ODAzMH0.AfpGFcYvVrS25qTr9RTGWqsvWMKykU2QcXZPtiNxAqY';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
